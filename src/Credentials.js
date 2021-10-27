16797c3a0262c8f6ab230b5fcbfbd075700b4d05
otienotilen <otienotilen@gmail.com>
2021-10-27 15:42:02 +0300

    first commit

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '1a9fe9f1a0fe46c59e704483e305cb7f',
+        ClientSecret: 'fe7b542e792540b796fd16239920bdb1'
+    }
+}
+
+export { Credentials };
